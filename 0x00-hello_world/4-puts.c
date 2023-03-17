d5d46448811bde6bb7f1554925477c5d0382197f
normanerina <nerinak22@gmail.com>
2023-03-17 15:55:54 -0700

    Write a command that print a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
-#!/bin/bash
-gcc -Wall -Wextra -pedantic -std=gnu89 -o a.out -xc - <<EOF && ./a.out && rm a.out
 #include <stdio.h>
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
     return (0);
 }
-EOF
 
