d3707677bbc7d321efa27cf45735787ae013aebb
normanerina <nerinak22@gmail.com>
2023-03-17 15:34:46 -0700

    Write a C program that print a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic "$CFILE" -o cisfun
+gcc -Wall -Wextra -pedantic -std=gnu89 -o a.out -xc - <<EOF && ./a.out && rm a.out
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+EOF
+
